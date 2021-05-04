1191f3493c04d355a21de0b2c50ec0024cfab3ff
J0hn1975 <johnwrafter@live.co.uk>
2021-05-04 18:04:43 +0000

    Added javascript for drop down menu

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -26,3 +26,21 @@ function decreaseCount(a, b) {
     input.value = value;
   }
 }
+
+// Dropdown menu
+var tickets = {
+    HackneyEmpire: ["Monday 6th", "Tuesday 7th", "Wednesday 8th"],
+    FirstDirectArena: ["Friday 10th", "Saturday 11th", "Monday 13th"],
+    TheSSEHydro: ["Wednesday 15th", "Thursday 16th", "Friday 17th", "Tea"]
+}
+
+    function changecat(value) {
+        if (value.length == 0) document.getElementById("category").innerHTML = "<option></option>";
+        else {
+            var catOptions = "";
+            for (categoryId in tickets[value]) {
+                catOptions += "<option>" + tickets[value][categoryId] + "</option>";
+            }
+            document.getElementById("category").innerHTML = catOptions;
+        }
+    }
