47f6656f7c1ac124635b6c329bd8e233704b6be3
J0hn1975 <johnwrafter@live.co.uk>
2021-05-08 00:38:25 +0000

    Removed unwanted functions

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,32 +1,3 @@
-// Show and hide div
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
-// Quantity Counter
-function increaseCount(a, b) {
-  var input = b.previousElementSibling;
-  var value = parseInt(input.value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  input.value = value;
-}
-
-function decreaseCount(a, b) {
-  var input = b.nextElementSibling;
-  var value = parseInt(input.value, 10);
-  if (value > 1) {
-    value = isNaN(value) ? 0 : value;
-    value--;
-    input.value = value;
-  }
-}
-
 // Dropdown menu
 var tickets = {
     HackneyEmpire: ["Monday 6th", "Tuesday 7th", "Wednesday 8th"],
@@ -51,21 +22,5 @@ var tickets = {
         }
     }
 
-// Collaspible
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
 
 
